6aacfb61b4bc784699e56df05828bc8686a16a12
limekh <167549380+limekh@users.noreply.github.com>
2024-11-23 12:47:12 +0400

    Update main.py

--- lab3/main.py
+++ lab3/main.py
@@ -3,7 +3,8 @@ import argparse
 from crop_image import crop_image
 from image_size import image_size
 from load_image import load_image
-from plot_histogram import plot_histogram
+from plot_histogram import create_histogram
+from plot_histogram import show_histogram
 from save_image import save_image
 from show_image import show_image
 
@@ -11,6 +12,7 @@ from show_image import show_image
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Image processing with OpenCV')
     parser.add_argument('image_path', type=str, help='Path to the input image')
+    parser.add_argument('-op', '--output_path', type=str, help='Path for save image')
     parser.add_argument('-s', '--size', type=int, nargs=2,
                         help='Width and height to crop the image from the top-left corner')
     return parser.parse_args()
@@ -24,14 +26,15 @@ def main():
         height, width = image_size(image)
         print(f'Default size of image: {height}x{width}')
 
-        plot_histogram(image)
+        hist = create_histogram(image)
+        show_histogram(hist)
 
         cropped_image = crop_image(image, args.size)
 
         show_image(image, title='Original Image')
         show_image(cropped_image, title='Cropped Image')
 
-        save_image(cropped_image)
+        save_image(args.output_path, cropped_image)
 
     except Exception as e:
         print(f"Error: {e}")
