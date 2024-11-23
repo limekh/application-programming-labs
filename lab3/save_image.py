c858b6193694aa81cd2611395841418981f887be
limekh <167549380+limekh@users.noreply.github.com>
2024-11-23 12:40:47 +0400

    Update save_image.py

--- lab3/save_image.py
+++ lab3/save_image.py
@@ -2,10 +2,11 @@ import cv2
 import numpy as np
 
 
-def save_image(image: np.ndarray):
+def save_image(output_path: str, image: np.ndarray):
     """
     save the image
+    :param output_path: path for saving
     :param image: image like numpy massive
     :return:
     """
-    cv2.imwrite("cropped.jpg", image)
+    cv2.imwrite(output_path, image)
