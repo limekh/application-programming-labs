a20ede42f3e6c2bfc3062888082d6bce3f84badf
limekh <167549380+limekh@users.noreply.github.com>
2024-11-23 12:35:34 +0400

    Update crop_image.py

--- lab3/crop_image.py
+++ lab3/crop_image.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 
-def crop_image(image: np.ndarray, size: tuple):
+def crop_image(image: np.ndarray, size: tuple) -> np.ndarray:
     """
 
     :param image: image like numpy massive
     :param size: height and width of image
-    :return:
+    :return: cropped image like numpy massive
     """
     if size is not None:
         width, height = size
