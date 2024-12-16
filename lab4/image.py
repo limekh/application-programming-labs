905ac6e04a1afb34ee1d54047e1daec4ba9bf239
limekh <167549380+limekh@users.noreply.github.com>
2024-12-16 14:51:10 +0400

    Update image.py

--- lab4/image.py
+++ lab4/image.py
@@ -2,21 +2,16 @@ import cv2
 import os
 
 
-def get_image_info(image_path) -> tuple:
+def get_image_info(image_path: str) -> tuple:
     """
     Get info of each image in folder - height, width and count of channels
     :param image_path:
     :return:
     """
-    height = []
-    width = []
-    channel_count = []
-    image_paths = [os.path.join(image_path, f) for f in os.listdir(image_path)]
+    img = cv2.imread(path_to_image)
 
-    for img in image_paths:
-        image = cv2.imread(img)
-        height.append(image.shape[0])
-        width.append(image.shape[1])
-        channel_count.append(image.shape[2])
+    if img is None:
+        raise FileNotFoundError(f"Image not found or cannot be opened: {path_to_image}")
 
-    return height, width, channel_count
+    height, width, channels = img.shape
+    return height, width, channels
